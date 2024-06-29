b3959f8247f0285148d26e04fd7033476adba098
PicturePark <109809242+PicturePark1101@users.noreply.github.com>
2024-06-30 00:03:17 +0900

    Pracitce (#2)
    * [feat] create entity class
    
    * [dependency] add spring jpa dependency
    
    * [feat] create application.yml
    
    * [remove] remove file

---
+++ src/main/java/com/example/demo/domain/Post.java
@@ -0,0 +1,30 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "stu_id")
+  private Student student;
+
+  private String title;
+
+  private String content;
+}
